8c206293b12b73cc946434834f55c39be5f31efd
mvolcy19 <volcma5@furman.edu>
2025-12-04 15:54:20 -0500

    step 1 of portfolio complete

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+import json
+import os 
+
+def load_projects():
+    data_file = os.path.join("data", "projects.json")
+
+    if not os.path.exists(data_file):
+        print(f"Warning: {data_file} not found. Creating empty projects list.")
+        return []
+    
+    with open(data_file, "r", encoding="utf=8") as file:
+        data = json.load(file) 
+        return data.get("projects", [])
+
 def main():
-    # Your program will start here
-    print("Welcome to [Your Project Name]!")
-    # Add your initial code here
-    pass
+   projects = load_projects()
+   print(f"Loaded{len(projects)} projects.")
+
+   for project in projects: 
+       print(f"- {project['title']},({project['category']})")
 
 if __name__ == "__main__":
     main()
